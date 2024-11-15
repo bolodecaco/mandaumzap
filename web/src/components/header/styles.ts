e35ccf0ccf1e6c13d80718f6164a46c46724ce61
Vitor Rafael <95248045+vitorqf@users.noreply.github.com>
2024-11-15 20:07:14 -0300

    feat: add header component (#40)

---
+++ web/src/components/header/styles.ts
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components'
+
+export const Wrapper = styled.header`
+  background-color: white;
+  border-radius: 0.75rem;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  padding: 0 1.5rem;
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 1rem;
+  font-weight: 600;
+`
