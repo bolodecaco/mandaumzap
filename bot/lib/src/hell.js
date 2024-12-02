2cc892a7eeaed833b98286a63779b912e87bfc73
todpig <luccasraffael6@gmail.com>
2024-12-02 10:45:44 -0300

    fix: remove build file

--- bot/lib/src/hell.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hell = hell;
-function hell() {
-    console.log("Hell from hell.ts");
-}
-//# sourceMappingURL=hell.js.map
