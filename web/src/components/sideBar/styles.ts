cbfbaf54530c4a622941b97c5ed53681bd8fbe70
RenanMaestrya <renanalmeida2.ra@gmail.com>
2024-12-13 20:59:44 -0300

    fix: eslint

--- web/src/components/sideBar/styles.ts
+++ web/src/components/sideBar/styles.ts
@@ -1,4 +1,4 @@
-import { css, styled } from 'styled-components'
+import { css, styled } from 'styled-components';
 
 export const Wrapper = styled.div`
   border-radius: 0.75rem;
@@ -11,13 +11,14 @@ export const Wrapper = styled.div`
   max-width: 4rem;
   display: flex;
   flex-direction: column;
-`
+`;
+
 export const Logo = styled.img`
   width: 2rem;
   height: 2rem;
   margin-bottom: 2rem;
   border-radius: 0.5rem;
-`
+`;
 
 export const Nav = styled.nav`
   display: flex;
@@ -25,17 +26,17 @@ export const Nav = styled.nav`
   align-items: center;
   justify-content: center;
   gap: 1.5rem;
-`
+`;
 
-export const NavItem = styled.a<{$active?: boolean}>`
+export const NavItem = styled.a<{ $active?: boolean }>`
   width: 1.5rem;
   height: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${({ theme, $active }) => 
+  color: ${({ theme, $active }) =>
     $active ? theme.colors.primary : theme.colors.secondary};
-`
+`;
 
 export const Footer = styled.footer`
   display: flex;
@@ -43,26 +44,29 @@ export const Footer = styled.footer`
   align-items: center;
   justify-content: center;
   gap: 1.5rem;
-`
-export const FooterItem = styled.a<{ 
-  $active?: boolean, 
-  $isLogout?: boolean 
+`;
+
+export const FooterItem = styled.a<{
+  $active?: boolean;
+  $isLogout?: boolean;
 }>`
-  color: ${({ theme, $isLogout, $active }) => 
-    $isLogout 
-      ? theme.colors.tertiary 
-      : ($active ? theme.colors.primary : theme.colors.secondary)
-  };
+  color: ${({ theme, $isLogout, $active }) =>
+    $isLogout
+      ? theme.colors.tertiary
+      : $active
+      ? theme.colors.primary
+      : theme.colors.secondary};
   width: 1.5rem;
   height: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
+
 export const WrapperNavs = styled.div`
   display: flex;
   height: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-`
+`;
