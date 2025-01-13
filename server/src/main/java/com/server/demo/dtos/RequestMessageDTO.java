79819176e6af47925c238151a5311ab1d22eb7bd
Vitor Rafael <rqfvitor@gmail.com>
2025-01-13 19:57:30 -0300

    feat: add validation for messages

--- server/src/main/java/com/server/demo/dtos/RequestMessageDTO.java
+++ server/src/main/java/com/server/demo/dtos/RequestMessageDTO.java
@@ -3,8 +3,9 @@ package com.server.demo.dtos;
 import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +17,15 @@ import lombok.NoArgsConstructor;
 public class RequestMessageDTO {
 
     @Schema(description = "Conteúdo da mensagem", example = "Mensagem de exemplo")
-    @NotEmpty(message = "O conteúdo da mensagem não pode ser vazio")
+    @NotBlank(message = "O conteúdo da mensagem não pode ser vazio")
+    @Size(min = 3, max = 10000, message = "O conteúdo da mensagem deve ter entre 3 e 10.000 caracteres")
     private String content;
 
     @Schema(description = "Identificador da lista de transmissão", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+    @NotNull(message = "É preciso informar o ID da lista de transmissão")
     private UUID broadcastListId;
 
     @Schema(description = "ID do usuário que irá enviar a mensagem", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
-    @NotNull(message = "O ID do usuário não pode ser nulo")
+    @NotNull(message = "É preciso informar o ID do dono da mensagem")
     private UUID ownerId;
 }
