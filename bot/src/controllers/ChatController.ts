341b4e01e249837db6abb548d42bc577c10e0564
Renan Andrade <renanandrade@MacBook-Air-de-Renan.local>
2025-02-11 10:08:20 -0300

    refactor: change how swagger doc works using yaml with all api routes

--- bot/src/controllers/ChatController.ts
+++ bot/src/controllers/ChatController.ts
@@ -4,57 +4,7 @@ import { GlobalException } from "../exception/GlobalException";
 
 const chatRouter = (sessionService: SessionService) => {
   const router = express.Router();
-  /**
-   * @swagger
-   * /api/chats/{userId}/{sessionId}:
-   *   get:
-   *     tags:
-   *       - Chat
-   *     summary: Lista de sessões
-   *     description: Retorna uma lista com os IDs das sessões ativas
-   *     parameters:
-   *       - in: path
-   *         name: userId
-   *         schema:
-   *           type: string
-   *           default: "userId"
-   *         required: true
-   *         description: ID de um usuário
-   *       - in: path
-   *         name: sessionId
-   *         schema:
-   *           type: string
-   *           default: "session"
-   *         required: true
-   *         description: ID da sessão
-   *       - in: query
-   *         name: token
-   *         schema:
-   *           type: string
-   *           default: "token"
-   *         required: true
-   *         description: Token para autenticação
-   *     responses:
-   *       200:
-   *         description: Sucesso
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: array
-   *               items:
-   *                 type: string
-   *                 example: sessionId
-   *       404:
-   *         description: Sessão não encontrada
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: object
-   *               properties:
-   *                 message:
-   *                   type: string
-   *                   example: Sessão não encontrada
-   */
+
   router.get("/chats/:userId/:sessionId", async (req, res): Promise<any> => {
     const { sessionId, userId } = req.params;
     if (!sessionId || !userId) {
@@ -76,6 +26,7 @@ const chatRouter = (sessionService: SessionService) => {
 
     return res.status(200).json(chats);
   });
+
   return router;
 };
 
--- bot/src/controllers/SessionController.ts
+++ bot/src/controllers/SessionController.ts
@@ -5,39 +5,6 @@ import { GlobalException } from "../exception/GlobalException";
 const sessionRouter = (sessionService: SessionService) => {
   const router = express.Router();
 
-  /**
-   * @swagger
-   * /api/sessions/{userId}:
-   *   get:
-   *     tags: [Session]
-   *     summary: Lista de sessões
-   *     description: Retorna uma lista com os IDs das sessões ativas
-   *     parameters:
-   *       - in: path
-   *         name: userId
-   *         schema:
-   *           type: string
-   *           example: "userId"
-   *         required: true
-   *         description: O ID único do usuário.
-   *       - in: query
-   *         name: token
-   *         schema:
-   *           type: string
-   *           example: "token"
-   *         required: true
-   *         description: Token para autenticação
-   *     responses:
-   *       200:
-   *         description: Sucesso
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: array
-   *               items:
-   *                 type: string
-   *                 example: "sessionId12345"
-   */
   router.get("/sessions/:userId", async (req, res): Promise<any> => {
     const { userId } = req.params;
     if (!userId) {
@@ -51,55 +18,6 @@ const sessionRouter = (sessionService: SessionService) => {
     return res.status(200).json(sessions);
   });
 
-  /**
-   * @swagger
-   * /api/sessions:
-   *   post:
-   *     tags: [Session]
-   *     summary: Cria ou inicializa uma sessão
-   *     description: Cria uma nova sessão e retorna o QR Code para conexão.
-   *     parameters:
-   *       - in: query
-   *         name: token
-   *         schema:
-   *           type: string
-   *           default: "token"
-   *         required: true
-   *         description: Token para autenticação.
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type: object
-   *             properties:
-   *               sessionId:
-   *                 type: string
-   *                 example: "session"
-   *                 description: O ID único para a sessão a ser criada.
-   *               userId:
-   *                 type: string
-   *                 example: "userId"
-   *                 description: Token de autenticação para a sessão.
-   *     responses:
-   *       200:
-   *         description: Sessão já existe e será inicializada
-   *       201:
-   *         description: >
-   *           Session criada com sucesso
-   *           OBS: Caso seja a primeira conexão, o userId deve ser undefined ou null.
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: object
-   *               properties:
-   *                 qrcode:
-   *                   type: string
-   *                   example: "https://example.com/qrcode.png"
-   *                   description: URL do QR Code para a conexão.
-   *       400:
-   *         description: Requisição inválida, já existe uma sessão com o ID fornecido.
-   */
   router.post("/sessions", async (req, res): Promise<any> => {
     const { sessionId, userId } = req.body;
     if (!sessionId || !userId) {
@@ -137,42 +55,6 @@ const sessionRouter = (sessionService: SessionService) => {
     return res.status(201).json({ qrcode });
   });
 
-  /**
-   * @swagger
-   * /api/sessions/close/{userId}/{sessionId}:
-   *   delete:
-   *     tags:
-   *       - Session
-   *     summary: Fecha uma sessão
-   *     description: Fecha uma sessão ativa.
-   *     parameters:
-   *       - in: query
-   *         name: token
-   *         schema:
-   *           type: string
-   *           default: "token"
-   *         required: true
-   *         description: Token para autenticação.
-   *       - in: path
-   *         name: userId
-   *         schema:
-   *           type: string
-   *           default: "userId"
-   *         required: true
-   *         description: O ID único do usuário.
-   *       - in: path
-   *         name: sessionId
-   *         schema:
-   *           type: string
-   *           default: "session"
-   *         required: true
-   *         description: O ID único da sessão a ser fechada.
-   *     responses:
-   *       200:
-   *         description: Sessão fechada com sucesso.
-   *       400:
-   *         description: A sessão não existe.
-   */
   router.delete("/sessions/close/:userId/:sessionId", async (req, res): Promise<any> => {
     const { sessionId, userId } = req.params;
     if (!sessionId) {
@@ -195,35 +77,6 @@ const sessionRouter = (sessionService: SessionService) => {
     return res.status(200).json({ message: "Sessão fechada" });
   });
 
-  /**
-   * @swagger
-   * /api/sessions/{userId}/{sessionId}:
-   *   delete:
-   *     tags:
-   *       - Session
-   *     summary: Deleta e fecha uma sessão
-   *     description: Fecha uma sessão ativa.
-   *     parameters:
-   *       - in: path
-   *         name: sessionId
-   *         schema:
-   *           type: string
-   *           default: "session"
-   *         required: true
-   *         description: O ID único da sessão a ser fechada.
-   *       - in: query
-   *         name: token
-   *         schema:
-   *           type: string
-   *           default: "token"
-   *         required: true
-   *         description: Token para autenticação.
-   *     responses:
-   *       200:
-   *         description: Sessão fechada com sucesso.
-   *       400:
-   *         description: A sessão não existe.
-   */
   router.delete("/sessions/:userId/:sessionId", async (req, res): Promise<any> => {
     const { sessionId, userId } = req.params;
     if (!sessionId) {
