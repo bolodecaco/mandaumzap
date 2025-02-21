3ae1eb9ccfca95e2a867567e03b472a80fdde06d
todpig <luccasraffael6@gmail.com>
2025-02-21 11:35:54 -0300

    feat: processing message sqs status

--- bot/src/@types/MessageSQSProps.ts
+++ bot/src/@types/MessageSQSProps.ts
@@ -1,11 +1,18 @@
+import { ConnectionStatus } from "./ConnectionStatus";
+
 export interface MessageToBeSentSQSProps {
-  body: MessageSQS;
+  body: MessageSQS | MessageConnectionStatusProps;
   messageGroupId: string;
-  type: "progress";
+  type: "progress" | "status";
 }
 
 type MessageSQS = {
   sentChats: number;
   unsentChats: number;
   totalChats: number;
+};
+
+type MessageConnectionStatusProps = {
+  status: ConnectionStatus;
+  sessionId: string;
 };
--- server/src/main/java/com/server/demo/consumer/MessageConsumer.java
+++ server/src/main/java/com/server/demo/consumer/MessageConsumer.java
@@ -1,32 +1,50 @@
 package com.server.demo.consumer;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 
 @Service
 public class MessageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
-    @SqsListener("${cloud.aws.sqs.queue-name}")
-    public void receiveMessage(String message) {
-        try {
-            logger.info("Mensagem recebida: {}", message);
-            processMessage(message);
-        } catch (Exception e) {
-            logger.error("Erro ao processar mensagem: {}", e.getMessage());
-            throw e;
+    @Autowired
+    private AmazonSQS amazonSQS;
+
+    @Value("${cloud.aws.sqs.queue-status-name}")
+    private String queueName;
+
+    @Scheduled(fixedDelay = 5000)
+    public void pollQueue() {
+        String queueUrl = amazonSQS.getQueueUrl(queueName).getQueueUrl();
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl)
+                .withMaxNumberOfMessages(10)
+                .withWaitTimeSeconds(5);
+
+        List<Message> messages = amazonSQS.receiveMessage(receiveMessageRequest).getMessages();
+
+        for (Message message : messages) {
+            try {
+                processMessage(message.getBody());
+                amazonSQS.deleteMessage(queueUrl, message.getReceiptHandle());
+            } catch (Exception e) {
+                logger.error("Erro ao processar mensagem: {}", e.getMessage());
+
+            }
         }
     }
 
     private void processMessage(String message) {
         logger.info("Processando mensagem: {}", message);
-    }
-
-    @SqsListener("${cloud.aws.sqs.queue-name}")
-    public void receiveObject(Object object) {
-        logger.info("Objeto recebido: {}", object);
     }
 }
